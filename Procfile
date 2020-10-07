c3539a64d16d5001389079c90a6a551a8f1c0b1b
limjun92 <limjun9205@naver.com>
2020-10-07 11:45:21 +0900

    ??

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn RunUPproject.wsgi --log-file -
-web: gunicorn RunUPproject.wsgi --timeout 60000
--- RunUPproject/settings.py
+++ RunUPproject/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': 'team8_database',
         'USER': 'team8',
         'PASSWORD': 'dla13579!!',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432'
     }
 }
